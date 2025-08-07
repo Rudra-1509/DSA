b74561ad3c34683b7c3b071789c8c1d81fefbd75
Rudranil Mondal <rudranilmn1509@gmail.com>
2025-08-07 23:54:45 +0530

    Time: 7 ms (29.23%) | Memory: 16 MB (18.81%) - LeetSync

--- 893-all-nodes-distance-k-in-binary-tree/all-nodes-distance-k-in-binary-tree.cpp
+++ 893-all-nodes-distance-k-in-binary-tree/all-nodes-distance-k-in-binary-tree.cpp
@@ -30,9 +30,35 @@ public:
         dfs(root->right,lev+1,k);
         dfs(parents[root],lev+1,k);
     }
+    void bfs(TreeNode* root,int k){
+        queue<TreeNode*> q;
+        int dist=0;
+        q.push(root);
+        visited.insert(root);
+        while(!q.empty()){
+            int size=q.size();
+            if(dist==k) break;
+            for(int i=0;i<size;i++){
+                TreeNode* cur=q.front();q.pop();
+                for(TreeNode* neighbour:{cur->left,cur->right,parents[cur]}){
+                    if(neighbour && !visited.count(neighbour)){
+                        q.push(neighbour);
+                        visited.insert(neighbour);
+                    }
+                }
+            }
+            dist++;
+        }
+
+        while(!q.empty()){
+            res.push_back(q.front()->val);
+            q.pop();
+        }
+    }
     vector<int> distanceK(TreeNode* root, TreeNode* target, int k) {
         storeParent(root,nullptr);
-        dfs(target,0,k);
+        //dfs(target,0,k);
+        bfs(target,k);
         return res;
     }
 };
