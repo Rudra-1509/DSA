4bb51e6d34c8da9649380b993183d5d36a8d9eb8
Rudranil Mondal <rudranilmn1509@gmail.com>
2025-08-11 10:48:13 +0530

    Time: 1 ms (21.64%) | Memory: 18.1 MB (59.97%) - LeetSync

---
+++ 733-flood-fill/flood-fill.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    void dfs(vector<vector<int>>& image, int sr, int sc, int color,int oldColor){
+        if(sr<0 || sr>=image.size() || sc<0 || sc>=image[0].size() || image[sr][sc]!=oldColor) return;
+        image[sr][sc]=color;
+        dfs(image,sr+1,sc,color,oldColor);
+        dfs(image,sr-1,sc,color,oldColor);
+        dfs(image,sr,sc+1,color,oldColor);
+        dfs(image,sr,sc-1,color,oldColor);
+    }
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
+        if(image[sr][sc]==color)    return image;
+        int oldColor=image[sr][sc];
+        dfs(image,sr,sc,color,oldColor);
+        return image;
+    }
+};
