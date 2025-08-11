8fccff640d17b5985d91c7c23ae8e918cd355258
Rudranil Mondal <rudranilmn1509@gmail.com>
2025-08-11 10:15:40 +0530

    Time: 5 ms (33.50%) | Memory: 31.6 MB (20.52%) - LeetSync

--- 695-max-area-of-island/max-area-of-island.cpp
+++ 695-max-area-of-island/max-area-of-island.cpp
@@ -1,5 +1,9 @@
 class Solution {
 public:
+    vector<vector<int>> nav={{1,0},{-1,0},{0,1},{0,-1}};
+    bool isValid(int r,int c,int m,int n){
+        return (r>=0 && r<m && c>=0 && c<n);
+    }
     int dfs(vector<vector<int>>& grid,int m,int n,int r,int c){
         if(r<0 || r>=m || c<0 || c>=n || grid[r][c]==0)   return 0;
         grid[r][c]=0;
@@ -8,13 +12,32 @@ public:
                     dfs(grid,m,n,r,c+1)+
                     dfs(grid,m,n,r,c-1);
     }
+    int bfs(vector<vector<int>>& grid,int m,int n,int r,int c){
+        queue<pair<int,int>> q;
+        q.push({r,c});
+        grid[r][c]=0;
+        int area=1;
+        while(!q.empty()){
+            pair<int,int> cur=q.front();q.pop();
+            for(int i=0;i<4;i++){
+                int newr=cur.first+nav[i][0];
+                int newc=cur.second+nav[i][1];
+                if(isValid(newr,newc,m,n) && grid[newr][newc]==1){
+                    q.push({newr,newc});
+                    grid[newr][newc]=0;
+                    area++;
+                }
+            }
+        }
+        return area;
+    }
     int maxAreaOfIsland(vector<vector<int>>& grid) {
         int m=grid.size(),n=grid[0].size();
         int maxArea=0;
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 if(grid[i][j]==1){
-                    int area=dfs(grid,m,n,i,j);
+                    int area=bfs(grid,m,n,i,j);
                     maxArea=max(maxArea,area);
                 }
             }
