49e07602503793cd79bfe12acea19077b9e33f00
Rudranil Mondal <rudranilmn1509@gmail.com>
2025-08-11 23:03:33 +0530

    Time: 2 ms (46.94%) | Memory: 19.7 MB (60.70%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.cpp
+++ 547-number-of-provinces/number-of-provinces.cpp
@@ -7,6 +7,20 @@ public:
                 dfs(isConnected,visited,j);
         }
     }
+    void bfs(vector<vector<int>>& isConnected,vector<bool>& visited,int i){
+        queue<int> q;
+        q.push(i);
+        visited[i]=true;
+        while(!q.empty()){
+            int cur=q.front();q.pop();
+            for(int j=0;j<visited.size();j++){
+                if(isConnected[cur][j]==1 && !visited[j]){
+                    visited[j]=true;
+                    q.push(j);
+                }
+            }
+        }
+    }
     int findCircleNum(vector<vector<int>>& isConnected) {
         int n=isConnected.size();
         vector<bool> visited(n,false);
@@ -14,7 +28,7 @@ public:
         for(int i=0;i<n;i++){
             if(!visited[i]){
                 count++;
-                dfs(isConnected,visited,i);
+                bfs(isConnected,visited,i);
             }
         }
         return count;
