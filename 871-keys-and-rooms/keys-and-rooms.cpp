136d2e61051b2e0a149a432d57d65a404fa883e8
Rudranil Mondal <rudranilmn1509@gmail.com>
2025-08-11 23:19:49 +0530

    Time: 0 ms (100.00%) | Memory: 14.5 MB (58.14%) - LeetSync

--- 871-keys-and-rooms/keys-and-rooms.cpp
+++ 871-keys-and-rooms/keys-and-rooms.cpp
@@ -8,10 +8,24 @@ public:
                 dfs(rooms,visited,key);
         }
     }
+    void bfs(vector<vector<int>>& rooms,vector<bool>& visited,int i){
+        queue<int> q;
+        q.push(i);
+        visited[i]=true;
+        while(!q.empty()){
+            int cur=q.front();q.pop();
+            for(int& key:rooms[cur]){
+                if(!visited[key]){
+                    visited[key]=true;
+                    q.push(key);
+                }
+            }
+        }
+    }
     bool canVisitAllRooms(vector<vector<int>>& rooms) {
         n=rooms.size();
         vector<bool> visited(n,false);
-        dfs(rooms,visited,0);
+        bfs(rooms,visited,0);
         for(bool flag:visited){
             if(!flag)   return false;
         }
