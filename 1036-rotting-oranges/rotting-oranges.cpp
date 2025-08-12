81441c90303e0cd38ca4079bb512c9dfff5f8df9
Rudranil Mondal <rudranilmn1509@gmail.com>
2025-08-12 11:28:21 +0530

    Time: 0 ms (100.00%) | Memory: 17.1 MB (33.31%) - LeetSync

---
+++ 1036-rotting-oranges/rotting-oranges.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    int m,n;
+    bool isValid(int i,int j){
+        return (i>=0 && i<m && j>=0 && j<n);
+    }
+
+    int orangesRotting(vector<vector<int>>& grid) {
+        m=grid.size(),n=grid[0].size();
+        queue<pair<int,int>> q;
+        vector<vector<int>> nav={{0,1},{0,-1},{1,0},{-1,0}};
+        int time=-1,freshOranges=0;
+        //push level 0 rotten oranges
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(grid[i][j]==2)
+                    q.push({i,j});
+                else if(grid[i][j]==1)
+                    freshOranges++;
+            }
+        }
+        //if there are no rotten oranges return 0;
+        if(q.empty() && freshOranges)   return -1;
+        if(q.empty())   return 0;
+        //start bfs
+        while(!q.empty()){
+            int size=q.size();
+            time++;
+            for(int i=0;i<size;i++){
+                pair<int,int> cur=q.front();q.pop();
+                for(int i=0;i<4;i++){
+                    int newr=cur.first+nav[i][0];
+                    int newc=cur.second+nav[i][1];
+                    if(isValid(newr,newc) && grid[newr][newc]==1){
+                        q.push({newr,newc});
+                        grid[newr][newc]=2;
+                        freshOranges--;
+                    }
+                }
+            }
+        }
+
+
+        return freshOranges==0? time:-1;
+       
+    }
+};
