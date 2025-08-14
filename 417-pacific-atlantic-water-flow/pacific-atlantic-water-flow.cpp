bf1e1d0a2d013c7b90e29062a0fff1cf677add56
Rudranil Mondal <rudranilmn1509@gmail.com>
2025-08-14 22:21:10 +0530

    Time: 16 ms (28.36%) | Memory: 34.1 MB (9.66%) - LeetSync

--- 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.cpp
+++ 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.cpp
@@ -14,18 +14,35 @@ public:
                 dfs(newi,newj,heights,visited);
         }
     }
+
+    void bfs(int i,int j,vector<vector<int>>& heights,vector<vector<bool>>& visited){
+        queue<pair<int,int>> q;
+        q.push({i,j});
+        visited[i][j]=true;
+        while(!q.empty()){
+            pair<int,int> cur=q.front();q.pop();
+            for(int k=0;k<4;k++){
+            int newi=cur.first+nav[k][0];
+            int newj=cur.second+nav[k][1];
+            if(isValid(newi,newj) && !visited[newi][newj] && heights[newi][newj]>=heights[cur.first][cur.second]){
+                visited[newi][newj]=true;
+                q.push({newi,newj});
+            }
+            }
+        }
+    }
     vector<vector<int>> pacificAtlantic(vector<vector<int>>& heights) {
         m=heights.size(),n=heights[0].size();
         vector<vector<int>> result;
         vector<vector<bool>> pac(m,vector<bool>(n,false));
         vector<vector<bool>> atl(m,vector<bool>(n,false));
         for(int i=0;i<m;i++){
-            dfs(i,0,heights,pac);
-            dfs(i,n-1,heights,atl);
+            bfs(i,0,heights,pac);
+            bfs(i,n-1,heights,atl);
         }
         for(int j=0;j<n;j++){
-            dfs(0,j,heights,pac);
-            dfs(m-1,j,heights,atl);
+            bfs(0,j,heights,pac);
+            bfs(m-1,j,heights,atl);
         }
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
