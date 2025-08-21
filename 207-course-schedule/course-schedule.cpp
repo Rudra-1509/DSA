0c98a170ebb239006d6e20030d1cb3ed1fea0a1f
Rudranil Mondal <rudranilmn1509@gmail.com>
2025-08-21 08:45:54 +0530

    Time: 0 ms (100.00%) | Memory: 19.2 MB (86.45%) - LeetSync

--- 207-course-schedule/course-schedule.cpp
+++ 207-course-schedule/course-schedule.cpp
@@ -1,17 +1,20 @@
 class Solution {
 public:
-    bool dfs(int node,vector<bool>& visited, vector<vector<int>>& adjList,vector<bool>& path){
-        visited[node]=true;
-        path[node]=true;
-        for(int& neighbour:adjList[node]){
-            if(!visited[neighbour]){
-                if(!dfs(neighbour,visited,adjList,path))     return false;
-            }
-            else if(path[neighbour])    return false;
+ bool dfs(int node, vector<bool>& visited, vector<vector<int>>& adj, vector<bool>& path) {
+        if (path[node]) return false;     // found a cycle
+        if (visited[node]) return true;   // already processed safely
+
+        visited[node] = true;
+        path[node] = true;
+
+        for (int neigh : adj[node]) {
+            if (!dfs(neigh,visited, adj,  path)) return false;
         }
-        path[node]=false;
+
+        path[node] = false; // backtrack
         return true;
     }
+
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
         vector<vector<int>> adjList(numCourses);
         for(auto& edge:prerequisites){
