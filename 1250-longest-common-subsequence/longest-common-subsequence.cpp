06414bb029b9fd1dcc91af5d176421cb99d2585a
Rudranil Mondal <rudranilmn1509@gmail.com>
2025-10-24 10:27:44 +0530

    Time: 23 ms (80.90%) | Memory: 27.5 MB (29.26%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -10,6 +10,17 @@ public:
         int m=text1.size();
         int n=text2.size();
         vector<vector<int>> dp(m+1,vector<int>(n+1,-1));
-        return helper(text1,text2,m,n,dp);
+        // return helper(text1,text2,m,n,dp);
+        
+        for(int i=0;i<m+1;i++)  dp[i][0]=0;
+        for(int i=0;i<n+1;i++)  dp[0][i]=0;
+        for(int i=1;i<m+1;i++){
+            for(int j=1;j<n+1;j++){
+                if(text1[i-1]==text2[j-1])  dp[i][j]=1+dp[i-1][j-1];
+                else    dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+            }
+        }
+
+        return dp[m][n];
     }
 };
