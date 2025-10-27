fe4a118660149fa7cdc5e90162e5dd5585cb5ad9
Rudranil Mondal <rudranilmn1509@gmail.com>
2025-10-27 10:27:35 +0530

    Time: 32 ms (59.22%) | Memory: 31.5 MB (59.82%) - LeetSync

---
+++ 1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int minInsertions(string s) {
+        int n=s.size();
+        string t=s;
+        reverse(t.begin(),t.end());
+        vector<vector<int>> dp(n+1,vector<int>(n+1,0));
+        for(int i=1;i<n+1;i++){
+            for(int j=1;j<n+1;j++){
+                if(s[i-1]==t[j-1])  dp[i][j]=1+dp[i-1][j-1];
+                else dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+            }
+        }
+        return n-dp[n][n];
+    }
+};
