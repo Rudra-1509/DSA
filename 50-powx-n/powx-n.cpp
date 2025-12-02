e12a50fe7109e669a86ebb62d7ca4523ba5c1e06
Rudranil Mondal <rudranilmn1509@gmail.com>
2025-12-02 16:38:57 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (43.21%) - LeetSync

--- 50-powx-n/powx-n.cpp
+++ 50-powx-n/powx-n.cpp
@@ -1,16 +1,17 @@
 class Solution {
 public:
-    double myPow(double x, long long n) {
+    double myPow(double x, int n) {
         if(x==1 || x==0)    return x;  
         if(n==0)    return 1;
+        long power=n;
         double ans=1;
         if(n<0){
-            n=-n;x=1/x;
+            power=-power;x=1/x;
         }
-        while(n>0){
-            if(n & 1)   ans*=x;
+        while(power>0){
+            if(power & 1)   ans*=x;
             x*=x;
-            n=n>>1;
+            power=power>>1;
         }
         return ans;
     }
