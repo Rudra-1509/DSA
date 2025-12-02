ad11d6a4624c322ed199db5ef82838c143ccf11f
Rudranil Mondal <rudranilmn1509@gmail.com>
2025-12-02 16:37:47 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (43.21%) - LeetSync

---
+++ 50-powx-n/powx-n.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    double myPow(double x, long long n) {
+        if(x==1 || x==0)    return x;  
+        if(n==0)    return 1;
+        double ans=1;
+        if(n<0){
+            n=-n;x=1/x;
+        }
+        while(n>0){
+            if(n & 1)   ans*=x;
+            x*=x;
+            n=n>>1;
+        }
+        return ans;
+    }
+};
