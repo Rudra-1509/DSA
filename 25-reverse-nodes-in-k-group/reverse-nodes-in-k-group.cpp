7edec20ba301c9d3e05c05701ef035d07d3cf1af
Rudranil Mondal <rudranilmn1509@gmail.com>
2025-12-06 00:56:35 +0530

    Time: 0 ms (100.00%) | Memory: 16.4 MB (69.69%) - LeetSync

---
+++ 25-reverse-nodes-in-k-group/reverse-nodes-in-k-group.cpp
@@ -0,0 +1,54 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* rev(ListNode* head){ 
+        ListNode *prev=nullptr, *cur=head; 
+        while(cur){ 
+            ListNode* next=cur->next; 
+            cur->next=prev; 
+            prev=cur; 
+            cur=next; 
+            } 
+            return prev; 
+        }
+    ListNode* reverseKGroup(ListNode* head, int k) {
+        if(!head)   return nullptr;
+        ListNode* cur=head;
+        for(int i=0;i<k;i++){
+            if(!cur)    return head;
+            cur=cur->next;
+        }
+        ListNode* curGrpHead=head; 
+        ListNode* prevGrpTail=nullptr;
+        cur=head; 
+        ListNode* newHead=nullptr; 
+        int grpMemCount=1; 
+        while(cur){ 
+            if(grpMemCount==k){ 
+                ListNode *nextGrpHead=cur->next; 
+                cur->next=nullptr; 
+                if(prevGrpTail) prevGrpTail->next=nullptr;
+                ListNode*revHead=rev(curGrpHead); 
+                if(prevGrpTail) prevGrpTail->next=revHead; 
+                else newHead=revHead; 
+                curGrpHead->next=nextGrpHead; 
+                prevGrpTail=curGrpHead; 
+                cur=curGrpHead; 
+                curGrpHead=nextGrpHead; 
+                grpMemCount=0; 
+            } 
+                cur=cur->next; 
+                grpMemCount++; 
+        } 
+        return newHead;
+    }
+};
