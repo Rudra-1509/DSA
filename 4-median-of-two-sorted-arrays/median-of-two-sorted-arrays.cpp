65ad9afe532f20caaf8e60592ccb7d0fa940a730
Rudranil Mondal <rudranilmn1509@gmail.com>
2025-12-18 20:34:41 +0530

    Time: 0 ms (100.00%) | Memory: 95.3 MB (42.03%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
@@ -6,7 +6,7 @@ public:
         int l=0,r=m;
         while(l<=r){
             int i=(l+r)/2;
-            int j=(m+n)/2 -i;
+            int j=(m+n+1)/2 -i;
             int AL=(i>0)?  nums1[i-1] : INT_MIN;
             int AR=(i<m)? nums1[i]:INT_MAX;
             int BL=(j>0)?  nums2[j-1] : INT_MIN;
@@ -14,14 +14,11 @@ public:
 
             if(AL<=BR && BL<=AR){
                 if((m+n)%2==0)  return (max(AL,BL)+min(AR,BR))/2.0;
-                else            return min(AR,BR);
+                else            return max(AL,BL);
             }
-            else if(AL>BR){
-                r=i-1;
-            }
-            else{
-                l=i+1;
-            }
+            else if(AL>BR)  r=i-1;
+            else            l=i+1;
+            
         }
         return 0.0;
     }
