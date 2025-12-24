0a45ca6b2860e4d5f66bb0e3727595108d3d2074
Rudranil Mondal <rudranilmn1509@gmail.com>
2025-12-25 01:51:38 +0530

    Time: 0 ms (100.00%) | Memory: 17.2 MB (33.29%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.cpp
+++ 1036-rotting-oranges/rotting-oranges.cpp
@@ -1,47 +1,39 @@
 class Solution {
 public:
-    int m,n;
-    bool isValid(int i,int j){
-        return (i>=0 && i<m && j>=0 && j<n);
+    vector<vector<int>> nav={{0,1},{0,-1},{1,0},{-1,0}};
+    bool isValid(int m,int n,int r,int c){
+        return r>=0 && r<m && c>=0 && c<n;
     }
-
     int orangesRotting(vector<vector<int>>& grid) {
-        m=grid.size(),n=grid[0].size();
+        int m=grid.size(),n=grid[0].size();
         queue<pair<int,int>> q;
-        static vector<vector<int>> nav={{0,1},{0,-1},{1,0},{-1,0}};
-        int time=-1,freshOranges=0;
-        //push level 0 rotten oranges
+        int freshOranges=0,time=0;
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                if(grid[i][j]==2)
+                if(grid[i][j]==2)   
                     q.push({i,j});
                 else if(grid[i][j]==1)
                     freshOranges++;
             }
         }
-        //if there are no rotten oranges or there are no oranges at all
-        if(q.empty() && freshOranges)   return -1;
-        if(q.empty())   return 0;
-        //start bfs
-        while(!q.empty()){
+        while(!q.empty() && freshOranges){
             int size=q.size();
             time++;
-            for(int i=0;i<size;i++){
-                pair<int,int> cur=q.front();q.pop();
-                for(int i=0;i<4;i++){
-                    int newr=cur.first+nav[i][0];
-                    int newc=cur.second+nav[i][1];
-                    if(isValid(newr,newc) && grid[newr][newc]==1){
-                        q.push({newr,newc});
-                        grid[newr][newc]=2;
+            while(size--){
+                auto [i,j]=q.front();q.pop();
+                for(auto &d:nav){
+                    int newi=i+d[0];
+                    int newj=j+d[1];
+                    if(isValid(m,n,newi,newj) && grid[newi][newj]==1){
+                        grid[newi][newj]=2;
                         freshOranges--;
+                        q.push({newi,newj});
                     }
                 }
             }
+
         }
-
-
-        return freshOranges? -1:time;
-       
+        if(freshOranges)    return -1;
+        else    return time;
     }
 };
