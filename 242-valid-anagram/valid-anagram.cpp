78eacd3e59ecaabd07d1ae94a7fa3dd88c01c88c
Rudranil Mondal <rudranilmn1509@gmail.com>
2025-12-27 23:57:00 +0530

    Time: 0 ms (100.00%) | Memory: 9.8 MB (47.22%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,9 +1,13 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        unordered_map<char,int> mpp1,mpp2;
+        if(s.size()!=t.size())  return false;
+        unordered_map<char,int> mpp1;
         for(char ch: s) mpp1[ch]++;
-        for(char ch: t) mpp2[ch]++;
-        return mpp1==mpp2;
+        for(char ch: t){
+            mpp1[ch]--;
+            if(mpp1[ch]==0) mpp1.erase(ch);
+        }
+        return mpp1.empty();
     }
 };
