888d8ecc8f56f8ea4420e24833058eb37f932750
Rudranil Mondal <rudranilmn1509@gmail.com>
2025-12-29 17:14:18 +0530

    Time: 7 ms (19.96%) | Memory: 9.9 MB (25.41%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -5,6 +5,7 @@ public:
         unordered_map<char,int> mpp1;
         for(char ch: s) mpp1[ch]++;
         for(char ch: t){
+            if(!mpp1.count(ch)) return false; 
             mpp1[ch]--;
             if(mpp1[ch]==0) mpp1.erase(ch);
         }
