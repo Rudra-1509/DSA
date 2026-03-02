6eb416f882752cf4a7c63f074fa9cc9b20ecf7dc
Rudranil Mondal <rudranilmn1509@gmail.com>
2026-03-02 17:38:38 +0530

    Time: 0 ms (100.00%) | Memory: 10.6 MB (99.69%) - LeetSync

--- 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.cpp
+++ 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.cpp
@@ -11,15 +11,42 @@
  */
 class Solution {
 public:
-    void dfs(TreeNode* root, vector<int>& result){
-        if(!root)   return;
-        result.push_back(root->val);
-        dfs(root->left,result);
-        dfs(root->right,result);
-    }
+    // void helper(TreeNode* root,vector<int>& res){
+    //     if(!root)   return;
+    //     res.push_back(root->val);
+    //     helper(root->left,res);
+    //     helper(root->right,res);
+    // }
+    // vector<int> preorderTraversal(TreeNode* root) {
+    //     vector<int> res;
+    //     helper(root,res);
+    //     return res;
+    // }
     vector<int> preorderTraversal(TreeNode* root) {
-        vector<int> result;
-        dfs(root,result);
-        return result;
+        vector<int> res;
+        TreeNode* cur=root;
+        while(cur){
+            if(!cur->left){
+                res.push_back(cur->val);
+                cur=cur->right;
+            }
+            else{
+                TreeNode* rightMost=cur->left;
+                while(rightMost->right && rightMost->right!=cur)
+                    rightMost=rightMost->right;
+                if(!rightMost->right){
+                    //attach and before going to left subtree push the root
+                    rightMost->right=cur;
+                    res.push_back(cur->val);
+                    cur=cur->left;
+                }
+                else{
+                    //sever and go right
+                    rightMost->right=nullptr;
+                    cur=cur->right;
+                }
+            }
+        }
+        return res;
     }
 };
