f287f154f12ea1ed18d4318d8b04cd8d5ad8d553
Rudranil Mondal <rudranilmn1509@gmail.com>
2026-03-20 23:38:41 +0530

    Time: 3 ms (15.15%) | Memory: 17.6 MB (39.74%) - LeetSync

--- 801-is-graph-bipartite/is-graph-bipartite.cpp
+++ 801-is-graph-bipartite/is-graph-bipartite.cpp
@@ -11,13 +11,36 @@ public:
         }
         return true;
     }
+    // bool isBipartite(vector<vector<int>>& graph) {
+    //     int V=graph.size();
+    //     vector<int> colors(V,-1);
+    //     for(int i=0;i<V;i++){
+    //         if(colors[i]==-1){
+    //             if(dfs(i,0,graph,colors)==false)
+    //                 return false;
+    //         }
+    //     }
+    //     return true;
+    // }
     bool isBipartite(vector<vector<int>>& graph) {
         int V=graph.size();
         vector<int> colors(V,-1);
         for(int i=0;i<V;i++){
             if(colors[i]==-1){
-                if(dfs(i,0,graph,colors)==false)
-                    return false;
+                queue<pair<int,int>> q;
+                q.push({i,0});
+                colors[i]=0;
+                while(!q.empty()){
+                    auto [cur,col]=q.front();q.pop();
+                    for(auto neigh:graph[cur]){
+                        if(colors[neigh]==-1){
+                            q.push({neigh,1-col});
+                            colors[neigh]=1-col;
+                        }
+                        if(colors[neigh]==col)
+                            return false;
+                    }
+                }
             }
         }
         return true;
