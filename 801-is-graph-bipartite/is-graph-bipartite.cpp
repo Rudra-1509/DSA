4f36e6f88426ce8b2640895d13e809ab33a0c257
Rudranil Mondal <rudranilmn1509@gmail.com>
2026-03-20 23:26:55 +0530

    Time: 0 ms (100.00%) | Memory: 17.3 MB (91.70%) - LeetSync

--- 801-is-graph-bipartite/is-graph-bipartite.cpp
+++ 801-is-graph-bipartite/is-graph-bipartite.cpp
@@ -1,44 +1,23 @@
 class Solution {
 public:
-    //1->black 0->white
-    int n;
-    bool bfs(int i,vector<vector<int>>& graph,vector<int>& colors,int color){
-            queue<int> q;
-            q.push(i);
-            colors[i]=color;
-            
-            while(!q.empty()){
-                int cur=q.front();q.pop();
-                for(int neighbour:graph[cur]){
-                    if(colors[neighbour]==-1){
-                        q.push(neighbour);
-                        colors[neighbour]=1-colors[cur];
-                    }
-                    else{
-                        if(colors[cur]==colors[neighbour])    return false;
-                    }
-                }
+    bool dfs(int node,int color,vector<vector<int>>& graph,vector<int>& colors){
+        colors[node]=color;
+        for(int neigh:graph[node]){
+            if(colors[neigh]==-1){
+                if(dfs(neigh,1-color,graph,colors)==false)  
+                    return false;
+            }
+            else if(colors[neigh]==colors[node])    return false;
         }
         return true;
     }
-    bool dfs(int i,vector<vector<int>>& graph,vector<int>& colors,int color){
-            colors[i]=color;
-            for(int neighbour:graph[i]){
-                if(colors[neighbour]==-1){
-                        if(!dfs(neighbour,graph,colors,1-color))     
-                            return false;   
-                }
-                else if(colors[i]==colors[neighbour])    return false;
-            }
-            return true;
-    }
     bool isBipartite(vector<vector<int>>& graph) {
-        n=graph.size();
-        vector<int> colors(n,-1);
-        // return bfs(graph,colors);
-        for(int i=0;i<n;i++){
+        int V=graph.size();
+        vector<int> colors(V,-1);
+        for(int i=0;i<V;i++){
             if(colors[i]==-1){
-                if(!bfs(i,graph,colors,1))  return false;
+                if(dfs(i,0,graph,colors)==false)
+                    return false;
             }
         }
         return true;
