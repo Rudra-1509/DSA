01c23e5dbc758e5081407007323d8cf15d3c08ec
Rudranil Mondal <rudranilmn1509@gmail.com>
2026-05-21 21:37:08 +0530

    Time: 12 ms (42.08%) | Memory: 11.7 MB (24.58%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.cpp
+++ 76-minimum-window-substring/minimum-window-substring.cpp
@@ -3,7 +3,7 @@ public:
     string minWindow(string s, string t) {
         unordered_map<char,int> mpp,window;
         for(char ch:t)  mpp[ch]++;
-        int left=0,st_index=0,len=INT_MAX,checklist=0;
+        int left=0,checklist=0,len=INT_MAX,st_index=0;
         for(int right=0;right<s.size();right++){
             window[s[right]]++;
             if(mpp.count(s[right]) && mpp[s[right]]==window[s[right]])
@@ -13,13 +13,13 @@ public:
                     len=right-left+1;
                     st_index=left;
                 }
-                char c=s[left];
-                window[c]--;
-                left++;
-                if(mpp.count(c) && mpp[c]>window[c])
+                window[s[left]]--;
+                if(mpp.count(s[left]) && window[s[left]]<mpp[s[left]])
                     checklist--;
+                left++;
             }
-        }
-        return len==INT_MAX? "" : s.substr(st_index,len);
+        }   
+        return len==INT_MAX? "" :s.substr(st_index,len);
+
     }
 };
