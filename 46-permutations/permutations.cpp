136da1e2bd747446a2f886de32b1b4d1249bd8f2
Rudranil Mondal <rudranilmn1509@gmail.com>
2026-05-23 17:27:08 +0530

    Time: 1 ms (41.25%) | Memory: 10.5 MB (87.60%) - LeetSync

--- 46-permutations/permutations.cpp
+++ 46-permutations/permutations.cpp
@@ -1,54 +1,20 @@
 class Solution {
 public:
-    // void helper(vector<pair<int,int>>& vis,vector<vector<int>>& res,vector<int>& path){
-    //     if(path.size()==vis.size()){
-    //         res.push_back(path);
-    //         return;
-    //     }
-    //     for(int i=0;i<vis.size();i++){
-    //         if(!vis[i].second){
-    //             vis[i].second=1;
-    //             path.push_back(vis[i].first);
-    //             helper(vis,res,path);
-    //             path.pop_back();
-    //             vis[i].second=0;
-    //         }
-    //     }
-    // }
-    // vector<vector<int>> permute(vector<int>& nums) {
-    //     vector<pair<int,int>> vis;
-    //     for(int i=0;i<nums.size();i++){
-    //         vis.push_back({nums[i],0});
-    //     }
-    //     vector<vector<int>> res;
-    //     vector<int> path;
-    //     helper(vis,res,path);
-    //     return res;
-    // }
-
-    void helper(vector<int>& nums,int ind,vector<vector<int>>& res){
-        if(ind>=nums.size()){
-            res.push_back(nums);
+    void helper(vector<int>& nums,int index,vector<vector<int>>& result)
+    {
+        if(index==nums.size()){
+            result.push_back(nums);
             return;
         }
-        for(int i=ind;i<nums.size();i++){
-            swap(nums[ind],nums[i]);
-            helper(nums,ind+1,res);
-            swap(nums[ind],nums[i]);
+        for(int i=index;i<nums.size();i++){
+            swap(nums[i],nums[index]);
+            helper(nums,index+1,result);
+            swap(nums[i],nums[index]);
         }
     }
-
-
     vector<vector<int>> permute(vector<int>& nums) {
-        vector<vector<int>> res;
-        helper(nums,0,res);
-        return res;
+        vector<vector<int>> result;
+        helper(nums,0,result);
+        return result;
     }
-
-
-
-
-
-
-
 };
