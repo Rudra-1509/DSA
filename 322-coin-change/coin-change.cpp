b3ebf3c6a2afda0608b2405d77c60cd01d9dd8ba
Rudranil Mondal <rudranilmn1509@gmail.com>
2026-05-25 19:53:51 +0530

    Time: 45 ms (33.22%) | Memory: 52.9 MB (15.17%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -5,7 +5,7 @@ public:
         vector<vector<int>> dp(n+1,vector<int>(amount+1,INT_MAX-1));
         for(int i=0;i<n+1;i++)  dp[i][0]=0;
         for(int i=1;i<amount+1;i++){
-            dp[0][i]=INT_MAX;
+            dp[0][i]=INT_MAX-1;
             if(i%coins[0]==0)   dp[1][i]=i/coins[0];
             else dp[1][i]=INT_MAX-1;
         }
