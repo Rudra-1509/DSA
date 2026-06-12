dac52097370e379e56638f3eee2fc9a2fa7fa7c2
Rudranil Mondal <rudranilmn1509@gmail.com>
2026-06-12 16:46:44 +0530

    Time: 43 ms (91.59%) | Memory: 19.8 MB (82.57%) - LeetSync

--- 127-word-ladder/word-ladder.cpp
+++ 127-word-ladder/word-ladder.cpp
@@ -1,38 +1,29 @@
 class Solution {
 public:
-    void generate_and_insert(queue<string>& q,unordered_set<string>& s,unordered_set<string>& vis,string cur){
-        int n=cur.size();
-        for(int i=0;i<n;i++){
-            for(int j=0;j<26;j++){
-                string newWord=cur;
-                newWord[i]='a'+j;
-                if(newWord!=cur && !vis.count(newWord) && s.count(newWord)){
-                    q.push(newWord);
-                    vis.insert(newWord);
-                }
-            }
-        }
-    }
     int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
-        bool flag=false;
-        unordered_set<string> s,vis;
-        s.insert(beginWord);
-        for(string word: wordList){
-            s.insert(word);
-            if(word==endWord)   flag=true;
-        }
-        if(!flag)   return 0;
+        unordered_set<string> s(wordList.begin(),wordList.end());
+        if(!s.count(endWord))   return 0;
         queue<string> q;
         q.push(beginWord);
-        vis.insert(beginWord);
+        s.erase(beginWord);
         int ans=0;
         while(!q.empty()){
             int sz=q.size();
             ans++;
-            for(int i=0;i<sz;i++){
+            while(sz--){
                 string cur=q.front();q.pop();
-                if(cur==endWord)    return ans;
-                generate_and_insert(q,s,vis,cur);
+                if(cur == endWord)      return ans;
+                for(int i=0;i<cur.size();i++){
+                    char ch=cur[i];
+                    for(int j=0;j<26;j++){
+                        cur[i]='a'+j;
+                        if(s.count(cur)){
+                            q.push(cur);
+                            s.erase(cur);
+                        }
+                    }
+                    cur[i]=ch;
+                }
             }
         }
         return 0;
