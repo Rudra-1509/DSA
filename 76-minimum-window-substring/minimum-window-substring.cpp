f4b43fa9768b9b85d457e504bf90959d75dd2dca
Rudranil Mondal <rudranilmn1509@gmail.com>
2026-06-13 12:04:02 +0530

    Time: 15 ms (36.20%) | Memory: 11.7 MB (25.84%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.cpp
+++ 76-minimum-window-substring/minimum-window-substring.cpp
@@ -1,25 +1,27 @@
 class Solution {
 public:
     string minWindow(string s, string t) {
+        if(t.size()>s.size())   return "";
         unordered_map<char,int> mpp,window;
         for(char ch:t)  mpp[ch]++;
-        int left=0,checklist=0,len=INT_MAX,st_index=0;
+        int len=INT_MAX,start,checklist=0,left=0;
         for(int right=0;right<s.size();right++){
-            window[s[right]]++;
-            if(mpp.count(s[right]) && mpp[s[right]]==window[s[right]])
+            char rc=s[right];
+            window[rc]++;
+            if(mpp.count(rc) && mpp[rc]==window[rc])    
                 checklist++;
             while(checklist==mpp.size()){
                 if(len>right-left+1){
                     len=right-left+1;
-                    st_index=left;
+                    start=left;
                 }
-                window[s[left]]--;
-                if(mpp.count(s[left]) && window[s[left]]<mpp[s[left]])
+                char lc=s[left];
+                window[lc]--;
+                if(mpp.count(lc) && window[lc]<mpp[lc]) 
                     checklist--;
                 left++;
             }
-        }   
-        return len==INT_MAX? "" :s.substr(st_index,len);
-
+        }
+        return len==(INT_MAX) ? "" :s.substr(start,len);
     }
 };
