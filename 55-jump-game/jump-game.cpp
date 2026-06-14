8e6ec65db80f779d48d3ad925e763d1763d4101f
Rudranil Mondal <rudranilmn1509@gmail.com>
2026-06-14 13:02:32 +0530

    Time: 0 ms (100.00%) | Memory: 52.4 MB (57.50%) - LeetSync

--- 55-jump-game/jump-game.cpp
+++ 55-jump-game/jump-game.cpp
@@ -2,13 +2,11 @@ class Solution {
 public:
     bool canJump(vector<int>& nums) {
         int n=nums.size(),farthest=0;
-        vector<bool> dp(n,false);
-        dp[0]=true;
         for(int i=0;i<n;i++){
-            for(int j=1;j<=nums[i] && (i+j)<n;j++){
-                dp[i+j]=dp[i+j]||dp[i];
-            }
+            if(i> farthest) return false;
+            farthest=max(farthest,i+nums[i]);
+            if(farthest==n-1)   return true;
         }
-        return dp[n-1];
+        return true;
     }
 };
