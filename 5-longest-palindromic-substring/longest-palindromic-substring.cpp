207be2bdafd071c1b7ed2436ea9340ed46ae521e
Rudranil Mondal <rudranilmn1509@gmail.com>
2026-06-15 19:24:38 +0530

    Time: 108 ms (25.65%) | Memory: 26.2 MB (29.03%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/longest-palindromic-substring.cpp
@@ -1,25 +1,42 @@
 class Solution {
 public:
-    int expand(string& s,int l,int r){
-        while(l>=0 && r<s.size() && s[l]==s[r]){
-            l--;
-            r++;
-        }
-        return r-l-1;
-    }
+    // string longestPalindrome(string s) {
+    //     if(s.empty())   return "";
+    //     int start=0,maxlen=INT_MIN;
+    //     for(int i=0;i<s.size();i++){
+    //         expandAroundCenter(s,i,i,start,maxlen);
+    //         expandAroundCenter(s,i,i+1,start,maxlen);
+    //     }
+    //     return s.substr(start,maxlen);
+    // }
+    // void expandAroundCenter(string& s,int left,int right,int& start,int& maxlen){
+    //     while(left>=0 && right<s.size() && s[left]==s[right]){
+    //         if(maxlen<right-left+1){
+    //             maxlen=right-left+1;
+    //             start=left;
+    //         }
+    //         left--;
+    //         right++;
+    //     }
+    //}
+
     string longestPalindrome(string s) {
-        int maxlen=0,start=0;
-        string ans;
-        for(int i=0;i<s.size();i++){
-            int len1=expand(s,i,i);
-            int len2=expand(s,i,i+1);
-            int maxi=max(len1,len2);
-            if(maxi>maxlen){
-                start=i-(maxi-1)/2;
-                maxlen=maxi;
+        int n=s.size(),start=0,maxlen=1;
+        vector<vector<bool>> isPal(n,vector<bool>(n,false));
+        for(int i=0;i<n;i++)    isPal[i][i]=true;
+        for(int len=2;len<=n;len++){
+            for(int i=0;i<=n-len;i++){
+                int j=i+len-1;
+                if(s[i]==s[j] &&(len<=3 || isPal[i+1][j-1])){
+                    isPal[i][j]=true;
+                    if(len>maxlen){
+                        maxlen=len;
+                        start=i;
+                    }
+                }
             }
         }
-        ans=s.substr(start,maxlen);
-        return ans;
+
+        return s.substr(start,maxlen);
     }
 };
