6d42aca3d8de80a12d26a56471b71bd7e9b327a6
Rudranil Mondal <rudranilmn1509@gmail.com>
2026-06-18 13:17:56 +0530

    Time: 4 ms (15.17%) | Memory: 18.1 MB (5.27%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.cpp
+++ 1036-rotting-oranges/rotting-oranges.cpp
@@ -1,37 +1,39 @@
 class Solution {
 public:
-    vector<vector<int>> nav={{0,1},{0,-1},{1,0},{-1,0}};
-    bool isValid(int m,int n,int r,int c){
-        return r>=0 && r<m && c>=0 && c<n;
+    int m,n;
+    bool isValid(int i,int j){
+        return (i>=0 && i<m && j>=0 && j<n);
     }
+    vector<vector<int>> nav={{0,1},{0,-1},{1,0},{-1,0}};
     int orangesRotting(vector<vector<int>>& grid) {
-        int row=grid.size(),col=grid[0].size(),freshOranges=0,time=0;
+        m=grid.size(),n=grid[0].size();
         queue<pair<int,int>> q;
-        for(int i=0;i<row;i++){
-            for(int j=0;j<col;j++){
-                if(grid[i][j]==2)
+        int freshOranges=0,time=0;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(grid[i][j]==2){
                     q.push({i,j});
-                if(grid[i][j]==1)
+                }
+                if(grid[i][j]==1){
                     freshOranges++;
+                }
             }
         }
-        while(freshOranges && !q.empty()){
+        while(!q.empty() && freshOranges){
             int sz=q.size();
             time++;
-            for(int l=0;l<sz;l++){
+            for(int lev=0;lev<sz;lev++){
                 auto [i,j]=q.front();q.pop();
-                for(int k=0;k<4;k++){
-                    int ni=i+nav[k][0];
-                    int nj=j+nav[k][1];
-                    if(isValid(row,col,ni,nj) && grid[ni][nj]==1){
-                        q.push({ni,nj});
+                for(auto d:nav){
+                    int ni=d[0]+i,nj=d[1]+j;
+                    if(isValid(ni,nj) && grid[ni][nj]==1){
                         grid[ni][nj]=2;
                         freshOranges--;
+                        q.push({ni,nj});
                     }
                 }
             }
         }
-        if(freshOranges)    return -1;
-        return time;
+        return freshOranges ? -1:time;
     }
 };
