1c7e98033c2facb78f7b2932a8a3bbd011b49623
Rudranil Mondal <rudranilmn1509@gmail.com>
2026-06-20 16:36:18 +0530

    Time: 47 ms (53.78%) | Memory: 57.8 MB (50.37%) - LeetSync

--- 131-palindrome-partitioning/palindrome-partitioning.cpp
+++ 131-palindrome-partitioning/palindrome-partitioning.cpp
@@ -1,21 +1,24 @@
 class Solution {
 public:
-    bool isPal(string s,int l,int r){
+    bool isPal(string w){
+        int l=0,r=w.size()-1;
         while(l<r){
-            if(s[l]!=s[r])  return false;
+            if(w[l]!=w[r])  return false;
             l++;r--;
         }
         return true;
     }
-    void helper(string s,int ind,vector<vector<string>>& res,vector<string>& path){
-        if(ind==s.size()){
+
+    void helper(string s,int start,vector<string>& path,vector<vector<string>>& res){
+        if(start==s.size()){
             res.push_back(path);
             return;
         }
-        for(int i=ind;i<s.size();i++){
-            if(isPal(s,ind,i)){
-                path.push_back(s.substr(ind,i-ind+1));
-                helper(s,i+1,res,path);
+        for(int end=start;end<s.size();end++){
+            string w=s.substr(start,end-start+1);
+            if(isPal(w)){
+                path.push_back(w);
+                helper(s,end+1,path,res);
                 path.pop_back();
             }
         }
@@ -23,7 +26,7 @@ public:
     vector<vector<string>> partition(string s) {
         vector<vector<string>> res;
         vector<string> path;
-        helper(s,0,res,path);
+        helper(s,0,path,res);
         return res;
     }
 };
