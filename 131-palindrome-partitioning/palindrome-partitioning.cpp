538ae5f79adc9a55e961a72f8ea15f401f9ce3c7
Rudranil Mondal <rudranilmn1509@gmail.com>
2026-06-20 16:52:15 +0530

    Time: 43 ms (58.62%) | Memory: 57.8 MB (50.37%) - LeetSync

--- 131-palindrome-partitioning/palindrome-partitioning.cpp
+++ 131-palindrome-partitioning/palindrome-partitioning.cpp
@@ -9,24 +9,42 @@ public:
         return true;
     }
 
-    void helper(string s,int start,vector<string>& path,vector<vector<string>>& res){
+    void helper(string s,int start,vector<string>& path,vector<vector<string>>& res,vector<vector<bool>>& dp){
         if(start==s.size()){
             res.push_back(path);
             return;
         }
         for(int end=start;end<s.size();end++){
-            string w=s.substr(start,end-start+1);
-            if(isPal(w)){
-                path.push_back(w);
-                helper(s,end+1,path,res);
+            // string w=s.substr(start,end-start+1);
+            if(dp[start][end]){
+            // if(isPal(w)){
+                // path.push_back(w);
+                path.push_back(s.substr(start,end-start+1));
+                helper(s,end+1,path,res,dp);
                 path.pop_back();
             }
         }
     }
+
+    void fillDP(string s,vector<vector<bool>>& dp){
+        int n=s.size();
+        for(int i=0;i<n;i++)    dp[i][i]=true;
+        for(int len=2;len<=n;len++){
+            for(int i=0;i<=n-len;i++){
+                int j=i+len-1;
+                if(s[i]==s[j] && (len<=3 || dp[i+1][j-1]))
+                    dp[i][j]=true;
+            }
+        }
+    }
+
     vector<vector<string>> partition(string s) {
         vector<vector<string>> res;
         vector<string> path;
-        helper(s,0,path,res);
+        int n=s.size();
+        vector<vector<bool>> dp(n,vector<bool>(n,false));
+        fillDP(s,dp);
+        helper(s,0,path,res,dp);
         return res;
     }
 };
