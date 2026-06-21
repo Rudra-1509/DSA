8848137590fa99976d857d79275af1b7f2287122
Rudranil Mondal <rudranilmn1509@gmail.com>
2026-06-21 10:38:30 +0530

    Time: 0 ms (100.00%) | Memory: 13.7 MB (97.23%) - LeetSync

--- 748-largest-number-at-least-twice-of-others/largest-number-at-least-twice-of-others.cpp
+++ 748-largest-number-at-least-twice-of-others/largest-number-at-least-twice-of-others.cpp
@@ -3,7 +3,7 @@ public:
     int dominantIndex(vector<int>& nums) {
         int first=INT_MIN,second=INT_MIN,idx=-1;
         for(int i=0;i<nums.size();i++){
-            if(nums[i]==first || nums[i]==second)   continue;
+            // if(nums[i]==first || nums[i]==second)   continue; not needed eg [3,3,1]
             if(nums[i]>first){
                 second=first;
                 first=nums[i];
