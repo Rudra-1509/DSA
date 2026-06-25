b5b5b0016fc2b18fd8acaae8c37c41e9ff82c109
Rudranil Mondal <rudranilmn1509@gmail.com>
2026-06-25 17:36:19 +0530

    Time: 56 ms (25.36%) | Memory: 93.9 MB (45.59%) - LeetSync

--- 452-minimum-number-of-arrows-to-burst-balloons/minimum-number-of-arrows-to-burst-balloons.cpp
+++ 452-minimum-number-of-arrows-to-burst-balloons/minimum-number-of-arrows-to-burst-balloons.cpp
@@ -2,14 +2,15 @@ class Solution {
 public:
     int findMinArrowShots(vector<vector<int>>& points) {
         sort(points.begin(),points.end());
-        int start=points[0][0],end=points[0][1], n=points.size(),ans=1;
+        // int start=points[0][0];
+        int end=points[0][1], n=points.size(),ans=1;
         for(int i=1;i<n;i++){
             if(points[i][0]<=end){
-                start=max(start,points[i][0]);
+                //start=max(start,points[i][0]);
                 end=min(end,points[i][1]);
             }
             else{
-                start=points[i][0];
+                //start=points[i][0];
                 end=points[i][1];
                 ans++;
             }
