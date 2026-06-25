1fec13a6d375298383d8b9773c9a634a02ebd6b8
Rudranil Mondal <rudranilmn1509@gmail.com>
2026-06-25 17:12:00 +0530

    Time: 0 ms (100.00%) | Memory: 18.8 MB (26.43%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.cpp
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.cpp
@@ -10,57 +10,37 @@
  */
 class Solution {
 public:
-    // ListNode* merge2Lists(ListNode* a,ListNode* b){
-    //     ListNode dummy(-1);
-    //     ListNode *cur1=a,*cur2=b,*cur=&dummy;
-    //     while(cur1 && cur2){
-    //         if(cur1->val<=cur2->val){
-    //             cur->next=cur1;
-    //             cur1=cur1->next;
-    //         }
-    //         else{
-    //             cur->next=cur2;
-    //             cur2=cur2->next;
-    //         }
-    //         cur=cur->next;
-    //     }
-    //     if(cur1)    cur->next=cur1;
-    //     else        cur->next=cur2;
-    //     return dummy.next;
-    // }
-
-    // ListNode* helper(vector<ListNode*>& lists,int low,int high){
-    //     if(low>high)    return nullptr;
-    //     if(low==high)   return lists[low];
-    //     int mid=low+(high-low)/2;
-    //     ListNode* lhs=helper(lists,low,mid);
-    //     ListNode* rhs=helper(lists,mid+1,high);
-    //     return merge2Lists(lhs,rhs);
-    // }
-
-    // ListNode* mergeKLists(vector<ListNode*>& lists) {   
-    //     return helper(lists,0,lists.size()-1);
-    // }
-    struct cmp{
-        bool operator()(ListNode* a,ListNode*b){
-            return a->val>b->val;
-        }
-    };
-    ListNode* mergeKLists(vector<ListNode*>& lists) { 
-        priority_queue<ListNode*,vector<ListNode*>,cmp> pq;
-        for(auto ll:lists){
-            if(ll)
-                pq.push(ll);
-        }
+    ListNode* merge2lists(ListNode* a,ListNode*  b){
         ListNode dummy(-1);
-        ListNode* cur=&dummy;
-        while(!pq.empty()){
-            ListNode* node=pq.top();pq.pop();
-            cur->next=node;
+        ListNode *cur1=a, *cur2=b, *cur=&dummy;
+        while(cur1 && cur2){
+            if(cur1->val<=cur2->val){
+                cur->next=cur1;
+                cur1=cur1->next;
+            }
+            else{
+                cur->next=cur2;
+                cur2=cur2->next;
+            }
             cur=cur->next;
-            if(node->next)  pq.push(node->next);
         }
-
+        if(cur1)    cur->next=cur1;
+        else        cur->next=cur2;
+    
         return dummy.next;
+    }
+
+    ListNode* helper(vector<ListNode*>& lists,int l,int r){
+        if(l>r) return nullptr;
+        if(l==r)    return lists[l];
+        int m=l+(r-l)/2;
+        ListNode* lhs=helper(lists,l,m);
+        ListNode* rhs=helper(lists,m+1,r);
+        return merge2lists(lhs,rhs);
+    }
+
+    ListNode* mergeKLists(vector<ListNode*>& lists) { 
+        int n=lists.size();
+        return helper(lists,0,n-1);
     }
 };
