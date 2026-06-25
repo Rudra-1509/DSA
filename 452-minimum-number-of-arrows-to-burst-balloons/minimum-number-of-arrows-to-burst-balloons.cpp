163ed589bad028d96ee1e5d5c2f4ac2d0a2400a1
Rudranil Mondal <rudranilmn1509@gmail.com>
2026-06-25 17:53:07 +0530

    Time: 2206 ms (5.01%) | Memory: 546.6 MB (5.03%) - LeetSync

--- 452-minimum-number-of-arrows-to-burst-balloons/minimum-number-of-arrows-to-burst-balloons.cpp
+++ 452-minimum-number-of-arrows-to-burst-balloons/minimum-number-of-arrows-to-burst-balloons.cpp
@@ -1,20 +1,33 @@
 class Solution {
 public:
     int findMinArrowShots(vector<vector<int>>& points) {
-        sort(points.begin(),points.end());
-        // int start=points[0][0];
-        int end=points[0][1], n=points.size(),ans=1;
+        // sort(points.begin(),points.end());
+        // // int start=points[0][0];
+        // int end=points[0][1], n=points.size(),ans=1;
+        // for(int i=1;i<n;i++){
+        //     if(points[i][0]<=end){
+        //         //start=max(start,points[i][0]);
+        //         end=min(end,points[i][1]);
+        //     }
+        //     else{
+        //         //start=points[i][0];
+        //         end=points[i][1];
+        //         ans++;
+        //     }
+        // }
+        // return ans;
+
+        sort(points.begin(),points.end(),[](auto a,auto b){
+            return a[1]<b[1];
+        });
+        int end=points[0][1],n=points.size(),ans=1;
         for(int i=1;i<n;i++){
-            if(points[i][0]<=end){
-                //start=max(start,points[i][0]);
-                end=min(end,points[i][1]);
-            }
-            else{
-                //start=points[i][0];
-                end=points[i][1];
+            if(points[i][0]>end){
                 ans++;
+                end=points[i][1];
             }
         }
         return ans;
+
     }
 };
