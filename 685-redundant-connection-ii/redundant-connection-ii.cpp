4ff1cfc2b17097c0ae2e8b4373402c383364639f
Rudranil Mondal <rudranilmn1509@gmail.com>
2026-06-27 11:40:15 +0530

    Time: 3 ms (28.95%) | Memory: 13.9 MB (39.24%) - LeetSync

---
+++ 685-redundant-connection-ii/redundant-connection-ii.cpp
@@ -0,0 +1,60 @@
+class DSU {
+private:
+    vector<int> par,size;
+public:
+    DSU(int n){
+        size.resize(n+1,1);
+        par.resize(n+1);
+        for(int i=1;i<=n;i++)
+            par[i]=i;
+        size[0]=-1;
+    }
+
+    int find(int u){
+        if(par[u]==u)   return u;
+        return par[u]=find(par[u]);
+    }
+
+    bool unite(int u,int v){
+        int pu=find(u);
+        int pv=find(v);
+        if(pu==pv)  return true;//same component
+        if(size[pu]>=size[pv]){
+            size[pu]+=size[pv];
+            par[pv]=pu;
+        }
+        else{
+            size[pv]+=size[pu];
+            par[pu]=pv;
+        }
+        return false;
+    }
+};
+class Solution {
+public:
+    vector<int> findRedundantDirectedConnection(vector<vector<int>>& edges) {
+        int edgesUsed=0,n=edges.size();
+        DSU dsu(n);
+        vector<int> candidate1,candidate2;
+        vector<int> par(n+1,0);
+        for(auto& edge:edges){
+            int u=edge[0],v=edge[1];
+            if(par[v]==0)   par[v]=u;
+            else{
+                candidate1={par[v],v};
+                candidate2=edge;
+                edge[1]=-1;
+            }
+        }
+
+        for(auto edge:edges){
+            int u=edge[0],v=edge[1];
+            if(v==-1)   continue;
+            if(dsu.unite(u,v)){
+                if(candidate1.empty())  return edge;//no candidates mean a simple cycle return last edge
+                return candidate1;
+            }          
+        }
+        return candidate2;
+    }
+};
