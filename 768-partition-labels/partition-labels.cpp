c76fae910c769c14a92d6d8498754abd49a5d8a5
Rudranil Mondal <rudranilmn1509@gmail.com>
2026-06-28 13:16:20 +0530

    Time: 0 ms (100.00%) | Memory: 9 MB (21.06%) - LeetSync

--- 768-partition-labels/partition-labels.cpp
+++ 768-partition-labels/partition-labels.cpp
@@ -1,34 +1,50 @@
 class Solution {
 public:
-    vector<int> partitionLabels(string s) {
-        unordered_map<char,pair<int,int>> mpp;
-        for(int i=0;i<s.size();i++){
-            if(!mpp.count(s[i]))
-                mpp[s[i]]={i,-1};
-        }
-        for(int i=s.size()-1;i>=0;i--){
-            if(mpp[s[i]].second==-1){
-                mpp[s[i]].second=i;
-            }
-        }
+    // vector<int> partitionLabels(string s) {
+    //     unordered_map<char,pair<int,int>> mpp;
+    //     for(int i=0;i<s.size();i++){
+    //         if(!mpp.count(s[i]))
+    //             mpp[s[i]]={i,-1};
+    //     }
+    //     for(int i=s.size()-1;i>=0;i--){
+    //         if(mpp[s[i]].second==-1){
+    //             mpp[s[i]].second=i;
+    //         }
+    //     }
 
-        vector<pair<int,int>> merged,intervals;
-        for(auto it:mpp)
-            intervals.push_back(it.second);
-        sort(intervals.begin(),intervals.end());
-        merged.push_back(intervals[0]);
-        for(int i=1;i<intervals.size();i++){
-            auto [s,e]=intervals[i];
-            if(s<merged.back().second){
-                merged.back().second=max(e,merged.back().second);
-            }
-            else{
-                merged.push_back({s,e});
+    //     vector<pair<int,int>> merged,intervals;
+    //     for(auto it:mpp)
+    //         intervals.push_back(it.second);
+    //     sort(intervals.begin(),intervals.end());
+    //     merged.push_back(intervals[0]);
+    //     for(int i=1;i<intervals.size();i++){
+    //         auto [s,e]=intervals[i];
+    //         if(s<merged.back().second){
+    //             merged.back().second=max(e,merged.back().second);
+    //         }
+    //         else{
+    //             merged.push_back({s,e});
+    //         }
+    //     }
+    //     vector<int> res;
+    //     for(auto&[l,r]: merged)
+    //         res.push_back(r-l+1);
+    //     return res;
+    // }
+    vector<int> partitionLabels(string s) {
+        unordered_map<char,int> mpp;
+        vector<int> res;
+        for(int i=0;i<s.size();i++){
+            mpp[s[i]]=i;
+        }
+        int start=0,end=0;
+        for(int i=0;i<s.size();i++){
+            end=max(end,mpp[s[i]]);
+            if(i==end){
+                res.push_back(end-start+1);
+                start=i+1;
             }
         }
-        vector<int> res;
-        for(auto&[l,r]: merged)
-            res.push_back(r-l+1);
         return res;
     }
 };
