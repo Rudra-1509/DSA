f56cae46a571d7d32d1e710776b360a5a94bd193
Rudranil Mondal <rudranilmn1509@gmail.com>
2026-06-29 11:36:57 +0530

    Time: 17 ms (85.32%) | Memory: 17.4 MB (54.94%) - LeetSync

--- 18-4sum/4sum.cpp
+++ 18-4sum/4sum.cpp
@@ -1,25 +1,24 @@
 class Solution {
 public:
-    vector<vector<int>> fourSum(vector<int>& nums, long long target) {
+    vector<vector<int>> fourSum(vector<int>& nums, int target) {
         sort(nums.begin(),nums.end());
         vector<vector<int>> res;
         int n=nums.size();
         for(int i=0;i<n-3;i++){
             if(i>0 && nums[i]==nums[i-1])   continue;
             for(int j=i+1;j<n-2;j++){
-                if(j>i+1 && nums[j]==nums[j-1]) continue;
-                long long newTarget=target-nums[i]-nums[j];
-                int l=j+1,r=n-1;
-                while(l<r){
-                    long long sum=nums[l]+nums[r];
-                    if(sum==newTarget){
-                        res.push_back({nums[i],nums[j],nums[l],nums[r]});
-                        l++;r--;
-                    while(l<r && nums[l]==nums[l-1])   l++;
-                    while(l<r && nums[r]==nums[r+1])   r--;
+                if(j>i+1 && nums[j]==nums[j-1])   continue;
+                int left=j+1,right=n-1;
+                while(left<right){
+                    long long rem=0LL+nums[left]+nums[right]+nums[i]+nums[j]-target;
+                    if(rem>0)        right--;
+                    else if(rem<0)   left++;
+                    else {
+                        res.push_back({nums[i],nums[j],nums[left],nums[right]});
+                        left++;right--;
+                        while(nums[left]==nums[left-1] && left<right) left++;
+                        while(nums[right]==nums[right+1] && left<right) right--;
                     }
-                    else if(sum<newTarget)   l++;
-                    else    r--;
                 }
             }
         }
