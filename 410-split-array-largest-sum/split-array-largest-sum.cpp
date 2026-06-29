f9ba4c9d50e1d484633dbe0fa4ea4685d9dcb19a
Rudranil Mondal <rudranilmn1509@gmail.com>
2026-06-29 12:12:27 +0530

    Time: 0 ms (100.00%) | Memory: 10.6 MB (42.41%) - LeetSync

--- 410-split-array-largest-sum/split-array-largest-sum.cpp
+++ 410-split-array-largest-sum/split-array-largest-sum.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    bool canSplit(vector<int>&nums,int k,int sum){
+    bool isValid(vector<int>& nums,int sum,int k){
         int sticks=0,curSum=0;
         for(int num:nums){
             curSum+=num;
@@ -9,19 +9,16 @@ public:
                 curSum=num;
             }
         }
-        return (sticks+1)<=k;
+        return sticks+1<=k;
     }
+
     int splitArray(vector<int>& nums, int k) {
         int left=*max_element(nums.begin(),nums.end());
         int right=accumulate(nums.begin(),nums.end(),0);
         while(left<right){
             int mid=left+(right-left)/2;
-            if(canSplit(nums,k,mid)){//this is a potential ans but we will try to find even smaller ans
-                right=mid;
-            }
-            else{//we r requiring much more sticks this means the sum or mid is too small compared to the nums
-                left=mid+1;
-            }
+            if(isValid(nums,mid,k)) right=mid;
+            else    left=mid+1;
         }
         return left;
     }
