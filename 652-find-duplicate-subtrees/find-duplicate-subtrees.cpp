12e1e43b83e19fb21772221ee8b502adc9f96ee1
Rudranil Mondal <rudranilmn1509@gmail.com>
2026-07-02 13:28:19 +0530

    Time: 16 ms (80.03%) | Memory: 27.3 MB (92.19%) - LeetSync

--- 652-find-duplicate-subtrees/find-duplicate-subtrees.cpp
+++ 652-find-duplicate-subtrees/find-duplicate-subtrees.cpp
@@ -11,19 +11,39 @@
  */
 class Solution {
 public:
-    string serialize(TreeNode* root,vector<TreeNode*>& res,unordered_map<string,int>& mpp){
-        if(!root)   return "#";
-        string lhs=serialize(root->left,res,mpp);
-        string rhs=serialize(root->right,res,mpp);
-        string cur=to_string(root->val)+',' + lhs + ',' + rhs;
-        if(++mpp[cur]==2)  res.push_back(root);
-        return cur;
+    // string serialize(TreeNode* root,vector<TreeNode*>& res,unordered_map<string,int>& mpp){
+    //     if(!root)   return "#";
+    //     string lhs=serialize(root->left,res,mpp);
+    //     string rhs=serialize(root->right,res,mpp);
+    //     string cur=to_string(root->val)+',' + lhs + ',' + rhs;
+    //     if(++mpp[cur]==2)  res.push_back(root);
+    //     return cur;
+    // }
+
+    // vector<TreeNode*> findDuplicateSubtrees(TreeNode* root) {
+    //     vector<TreeNode*> res;
+    //     unordered_map<string,int> mpp;
+    //     serialize(root,res,mpp);
+    //     return res;
+    // }
+    unordered_map<int,int> frq;
+    unordered_map<string,int> subTree;
+    vector<TreeNode*> res;
+    int Id=1;
+    int dfs(TreeNode* root){
+        if(!root)   return 0;
+        int lhs=dfs(root->left);
+        int rhs=dfs(root->right);
+        string key=to_string(root->val)+","+to_string(lhs)+","+to_string(rhs);
+        if(!subTree.count(key))
+            subTree[key] = Id++;
+        int curId=subTree[key];
+        if(++frq[curId]==2)    res.push_back(root);
+        return curId; 
     }
 
     vector<TreeNode*> findDuplicateSubtrees(TreeNode* root) {
-        vector<TreeNode*> res;
-        unordered_map<string,int> mpp;
-        serialize(root,res,mpp);
+        dfs(root);
         return res;
     }
 };
