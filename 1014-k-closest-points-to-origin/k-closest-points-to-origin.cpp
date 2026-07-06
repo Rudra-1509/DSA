2d09a5fd295f04044271ed2227cccf73b8dd1792
Rudranil Mondal <rudranilmn1509@gmail.com>
2026-07-06 11:09:07 +0530

    Time: 77 ms (38.61%) | Memory: 78.2 MB (26.88%) - LeetSync

--- 1014-k-closest-points-to-origin/k-closest-points-to-origin.cpp
+++ 1014-k-closest-points-to-origin/k-closest-points-to-origin.cpp
@@ -1,20 +1,12 @@
 class Solution {
-private:
-    struct cmp{
-        bool operator()(vector<int>& a, vector<int>& b){
-            long long dist1=a[0]*a[0]+a[1]*a[1];
-            long long dist2=b[0]*b[0]+b[1]*b[1];
-            return dist1<dist2;
-        }
-    };
 public:
     vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
         vector<vector<int>> res;
-        priority_queue<pair<long long,vector<int>>> pq;
-        for(auto& point: points){
-            long long dist=1LL*point[0]*point[0]+1LL*point[1]*point[1];
-            pq.push({dist,point});
-            if(pq.size()>k) pq.pop();
+        priority_queue<pair<long long,vector<int> >> pq;
+        for(auto& p:points){
+            int dist=1LL*p[0]*p[0]+p[1]*p[1];
+            pq.push({dist,p});
+            if(pq.size()>k)     pq.pop();
         }
         while(!pq.empty()){
             res.push_back(pq.top().second);
