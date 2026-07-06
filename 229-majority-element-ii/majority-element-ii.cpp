bef82eb2567d712073f027d817a66b2a39c34862
Rudranil Mondal <rudranilmn1509@gmail.com>
2026-07-06 12:16:31 +0530

    Time: 3 ms (50.68%) | Memory: 23.8 MB (44.54%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.cpp
+++ 229-majority-element-ii/majority-element-ii.cpp
@@ -1,23 +1,31 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        int count1=0,count2=0,pw1=-1,pw2=-1,n=nums.size();
-        for(int x: nums){
-            if(x==pw1)          count1++;
-            else if(x==pw2)     count2++;
-            else if(count1==0)  pw1=x,count1=1;
-            else if(count2==0)  pw2=x,count2=1;
-            else                count1--,count2--;
+        vector<int> res;
+        int candidate1=-1,candidate2=-1,count1=0,count2=0;
+        for(int& x:nums){
+            if(x==candidate1)   count1++;
+            else if(x==candidate2)  count2++;
+            else if(count1==0){
+                candidate1=x;
+                count1=1;
+            }  
+            else if(count2==0){
+                candidate2=x;
+                count2=1;
+            }
+            else{
+                count1--;count2--;
+            }
+        }
+        count1=count2=0;
+        for(int& x:nums){
+            if(x==candidate1)   count1++;
+            else if(x==candidate2)   count2++;
         }
 
-        count1=count2=0;
-        for(int x:nums){
-            if(x==pw1)  count1++;
-            else if(x==pw2)  count2++;
-        }
-        vector<int> res;
-        if(count1 > n/3)    res.push_back(pw1);
-        if(count2 > n/3)    res.push_back(pw2);
-        return res;    
+        if(count1>nums.size()/3)    res.push_back(candidate1);
+        if(count2>nums.size()/3)    res.push_back(candidate2);
+        return res;
     }
 };
