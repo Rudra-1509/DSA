2319d3a09735040eeb597e9dd2793c31155abb90
Rudranil Mondal <rudranilmn1509@gmail.com>
2026-07-06 12:23:46 +0530

    Time: 11 ms (26.48%) | Memory: 13.3 MB (37.05%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.cpp
+++ 13-roman-to-integer/roman-to-integer.cpp
@@ -10,15 +10,11 @@ public:
             {'D',500},
             {'M',1000}
         };
-
-        int ans=0;
-        for(int i=0;i<s.size();i++){
-            int cur=mpp[s[i]];
-            int next=(i+1<s.size())? mpp[s[i+1]]:0;
-            if(cur<next)    ans-=cur;
-            else            ans+=cur;
+        int ans=0,n=s.size();
+        for(int i=0;i<n;i++){
+            if(i<n-1 && mpp[s[i]]<mpp[s[i+1]])    ans+=-mpp[s[i]];
+            else    ans+=mpp[s[i]];
         }
-
         return ans;
     }
 };
