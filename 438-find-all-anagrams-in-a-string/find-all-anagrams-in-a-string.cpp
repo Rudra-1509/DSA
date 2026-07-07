bab41b613256d0ec525700212f98823b11929bfa
Rudranil Mondal <rudranilmn1509@gmail.com>
2026-07-07 12:06:45 +0530

    Time: 27 ms (17.17%) | Memory: 15.5 MB (14.28%) - LeetSync

--- 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.cpp
+++ 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.cpp
@@ -3,15 +3,14 @@ public:
     vector<int> findAnagrams(string s, string p) {
         unordered_map<char,int> mpp,window;
         vector<int> res;
-        for(char ch:p)  mpp[ch]++;
         int k=p.size();
+        for(char& ch:p)     mpp[ch]++;
         for(int i=0;i<s.size();i++){
             window[s[i]]++;
             if(i>=k-1){
-                if(mpp==window)
-                    res.push_back(i-k+1);
-                window[s[i-k+1]]--;
-                if(window[s[i-k+1]]==0) window.erase(s[i-k+1]);
+                if(window==mpp) res.push_back(i-k+1);
+                char ch=s[i-k+1];
+                if(--window[ch]==0) window.erase(ch);
             }
         }
         return res;
