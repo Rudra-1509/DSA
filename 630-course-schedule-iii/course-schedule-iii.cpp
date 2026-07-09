9ac5e3627da82f3f0c439e69622bbccd947dcf06
Rudranil Mondal <rudranilmn1509@gmail.com>
2026-07-09 20:49:27 +0530

    Time: 17 ms (89.13%) | Memory: 60.3 MB (92.67%) - LeetSync

--- 630-course-schedule-iii/course-schedule-iii.cpp
+++ 630-course-schedule-iii/course-schedule-iii.cpp
@@ -4,26 +4,22 @@ public:
         sort(courses.begin(),courses.end(),[](auto& a,auto& b){
             return a[1]<b[1];
         });
-        int curDay=0,courseTaken=0;
+        int curDay=0;
         priority_queue<int> pq;
         for(auto& course:courses){
             int dur=course[0],deadline=course[1];
             if(curDay+dur<=deadline){
                 curDay+=dur;
-                courseTaken++;
                 pq.push(course[0]);
             }
-            else{
-                if(!pq.empty() && dur>pq.top()) continue;
-                if(!pq.empty() && curDay+dur>deadline){
-                    int longest=pq.top();
-                    pq.pop();
-                    curDay-=longest;
-                    curDay+=dur;
-                    pq.push(dur);
-                }
+            else if(!pq.empty() && dur<pq.top()){
+                int longest=pq.top();
+                pq.pop();
+                curDay-=longest;
+                curDay+=dur;
+                pq.push(dur);
             }
         }
-        return courseTaken;
+        return pq.size();
     }
 };
