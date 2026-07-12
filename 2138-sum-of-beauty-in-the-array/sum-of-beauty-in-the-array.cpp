3c444b7a1fe381d20f0bd9dab65e249261e3d482
Rudranil Mondal <rudranilmn1509@gmail.com>
2026-07-12 21:42:09 +0530

    Time: 16 ms (13.01%) | Memory: 92.9 MB (45.06%) - LeetSync

--- 2138-sum-of-beauty-in-the-array/sum-of-beauty-in-the-array.cpp
+++ 2138-sum-of-beauty-in-the-array/sum-of-beauty-in-the-array.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int sumOfBeauties(vector<int>& nums) {
         int n=nums.size(),ans=0;
-        vector<int> preMax(n,0);
+        vector<int> preMax(n,INT_MIN);
         vector<int> preMin(n,INT_MAX);
         for(int i=1;i<n;i++)
             preMax[i]=max(nums[i-1],preMax[i-1]);
