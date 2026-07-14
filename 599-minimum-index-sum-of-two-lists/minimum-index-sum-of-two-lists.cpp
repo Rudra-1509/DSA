a8f1d5ca1cee8b487e43777c1443618bda0614ec
Rudranil Mondal <rudranilmn1509@gmail.com>
2026-07-14 23:03:19 +0530

    Time: 30 ms (34.94%) | Memory: 41.4 MB (27.91%) - LeetSync

--- 599-minimum-index-sum-of-two-lists/minimum-index-sum-of-two-lists.cpp
+++ 599-minimum-index-sum-of-two-lists/minimum-index-sum-of-two-lists.cpp
@@ -1,18 +1,22 @@
 class Solution {
 public:
     vector<string> findRestaurant(vector<string>& list1, vector<string>& list2) {
-        int m=list1.size(),n=list2.size(),sum=INT_MAX;
+        int m=list1.size(),n=list2.size(),best=INT_MAX;
         vector<string> res;
         unordered_map<string,int> mpp;
         for(int i=0;i<m;i++)
             mpp[list1[i]]=i;
         for(int i=0;i<n;i++){
-            if(mpp.count(list2[i]))
-                sum=min(sum,mpp[list2[i]]+i);
-        }
-        for(int i=0;i<n;i++){
-            if(mpp.count(list2[i]) && (mpp[list2[i]]+i)==sum)
+            if(!mpp.count(list2[i]))    continue;
+            int cur=mpp[list2[i]]+i;
+            if(best>cur){
+                res.clear();
                 res.push_back(list2[i]);
+                best=cur;
+            }
+            else if(best==cur){
+                res.push_back(list2[i]);
+            }
         }
         return res;
     }
