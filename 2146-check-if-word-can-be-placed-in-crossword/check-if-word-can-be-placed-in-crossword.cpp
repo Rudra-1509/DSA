5771aa52ca40a84c045f411fbd099b1784acc277
Rudranil Mondal <rudranilmn1509@gmail.com>
2026-07-16 20:30:46 +0530

    Time: 254 ms (10.76%) | Memory: 411 MB (17.72%) - LeetSync

--- 2146-check-if-word-can-be-placed-in-crossword/check-if-word-can-be-placed-in-crossword.cpp
+++ 2146-check-if-word-can-be-placed-in-crossword/check-if-word-can-be-placed-in-crossword.cpp
@@ -21,8 +21,11 @@ public:
                 if(board[i][j]=='#')    continue;
                 bool ans=false;
                 if(j==0 || board[i][j-1]=='#')      ans=ans || moveNext(i,j,0,1,board,word); //right
+                if(ans)     return true;
                 if(i==0 || board[i-1][j]=='#')      ans=ans || moveNext(i,j,1,0,board,word); //down
+                if(ans)     return true;
                 if(j==n-1 || board[i][j+1]=='#')    ans=ans || moveNext(i,j,0,-1,board,word); //left
+                if(ans)     return true;
                 if(i==m-1 || board[i+1][j]=='#')    ans=ans || moveNext(i,j,-1,0,board,word); //up
                 if(ans)     return true;
             }
