8200535a9dd9f1db322ce8c890832b4b7b15c7fa
Rudranil Mondal <rudranilmn1509@gmail.com>
2026-07-16 20:47:20 +0530

    Time: 2 ms (79.75%) | Memory: 64 MB (84.81%) - LeetSync

--- 2146-check-if-word-can-be-placed-in-crossword/check-if-word-can-be-placed-in-crossword.cpp
+++ 2146-check-if-word-can-be-placed-in-crossword/check-if-word-can-be-placed-in-crossword.cpp
@@ -4,7 +4,7 @@ public:
     bool isValid(int i,int j){
         return i>=0 && i<m && j>=0 && j<n;
     }
-    bool moveNext(int i,int j,int di,int dj,vector<vector<char>>& board, string& word){
+    bool moveNext(int i,int j,int di,int dj,vector<vector<char>>& board,const string& word){
         for(int k=0;k<word.size();k++){
             if(!isValid(i,j))   return false;
             if(board[i][j]!=' ' && board[i][j]!=word[k])    return false;
