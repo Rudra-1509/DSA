dc58b4cd19f3deecca617b96530f55a90840145f
Rudranil Mondal <rudranilmn1509@gmail.com>
2026-07-16 20:30:08 +0530

    Time: 261 ms (10.76%) | Memory: 410.8 MB (17.72%) - LeetSync

---
+++ 2146-check-if-word-can-be-placed-in-crossword/check-if-word-can-be-placed-in-crossword.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int m,n;
+    bool isValid(int i,int j){
+        return i>=0 && i<m && j>=0 && j<n;
+    }
+    bool moveNext(int i,int j,int di,int dj,vector<vector<char>>& board, string word){
+        for(int k=0;k<word.size();k++){
+            if(!isValid(i,j))   return false;
+            if(board[i][j]!=' ' && board[i][j]!=word[k])    return false;
+            i=i+di;
+            j=j+dj;
+        }
+        return ((isValid(i,j) && board[i][j]=='#') || !isValid(i,j));
+    }
+
+    bool placeWordInCrossword(vector<vector<char>>& board, string word) {
+        m=board.size(),n=board[0].size();
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(board[i][j]=='#')    continue;
+                bool ans=false;
+                if(j==0 || board[i][j-1]=='#')      ans=ans || moveNext(i,j,0,1,board,word); //right
+                if(i==0 || board[i-1][j]=='#')      ans=ans || moveNext(i,j,1,0,board,word); //down
+                if(j==n-1 || board[i][j+1]=='#')    ans=ans || moveNext(i,j,0,-1,board,word); //left
+                if(i==m-1 || board[i+1][j]=='#')    ans=ans || moveNext(i,j,-1,0,board,word); //up
+                if(ans)     return true;
+            }
+        }
+        return false;
+    }
+};
