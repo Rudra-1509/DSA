a04760a164bfbc3a9e7b127a6c69f17435ef8ed7
Rudranil Mondal <rudranilmn1509@gmail.com>
2026-07-16 20:39:30 +0530

    Time: 259 ms (10.76%) | Memory: 410.9 MB (17.72%) - LeetSync

--- 2146-check-if-word-can-be-placed-in-crossword/check-if-word-can-be-placed-in-crossword.cpp
+++ 2146-check-if-word-can-be-placed-in-crossword/check-if-word-can-be-placed-in-crossword.cpp
@@ -20,14 +20,10 @@ public:
             for(int j=0;j<n;j++){
                 if(board[i][j]=='#')    continue;
                 bool ans=false;
-                if(j==0 || board[i][j-1]=='#')      ans=ans || moveNext(i,j,0,1,board,word); //right
-                if(ans)     return true;
-                if(i==0 || board[i-1][j]=='#')      ans=ans || moveNext(i,j,1,0,board,word); //down
-                if(ans)     return true;
-                if(j==n-1 || board[i][j+1]=='#')    ans=ans || moveNext(i,j,0,-1,board,word); //left
-                if(ans)     return true;
-                if(i==m-1 || board[i+1][j]=='#')    ans=ans || moveNext(i,j,-1,0,board,word); //up
-                if(ans)     return true;
+                if((j==0 || board[i][j-1]=='#') && moveNext(i,j,0,1,board,word))     return true; //right
+                if((i==0 || board[i-1][j]=='#') && moveNext(i,j,1,0,board,word))     return true; //down
+                if((j==n-1 || board[i][j+1]=='#') && moveNext(i,j,0,-1,board,word))     return true; //left
+                if((i==m-1 || board[i+1][j]=='#') && moveNext(i,j,-1,0,board,word))     return true; //up
             }
         }
         return false;
