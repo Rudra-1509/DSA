37ffc32a38f08c7b3f955d55668d33cdaa78fe27
Rudranil Mondal <rudranilmn1509@gmail.com>
2026-07-17 21:12:49 +0530

    Time: 10 ms (46.77%) | Memory: 12.6 MB (55.83%) - LeetSync

--- 2134-maximize-the-confusion-of-an-exam/maximize-the-confusion-of-an-exam.cpp
+++ 2134-maximize-the-confusion-of-an-exam/maximize-the-confusion-of-an-exam.cpp
@@ -1,13 +1,20 @@
 class Solution {
 public:
     int maxConsecutiveAnswers(string answerKey, int k) {
-        unordered_map<char,int> mpp;
-        int left=0,ans=0,maxfrq=0;
+        //unordered_map<char,int> mpp;
+        int left=0,ans=0,maxfrq=0,countT=0,countF=0;
         for(int right=0;right<answerKey.size();right++){
-            mpp[answerKey[right]]++;
-            maxfrq=max(maxfrq,mpp[answerKey[right]]);
-            while(right-left+1-maxfrq>k)
-                mpp[answerKey[left++]]--;
+            // mpp[answerKey[right]]++;
+            if(answerKey[right]=='T')   countT++;
+            else                        countF++;
+            // maxfrq=max(maxfrq,mpp[answerKey[right]]);
+            // while(right-left+1-maxfrq>k)
+            //     mpp[answerKey[left++]]--;
+            while(min(countT,countF)>k){
+                if(answerKey[left]=='T')   countT--;
+                else    countF--;
+                left++;
+            }
             ans=max(ans,right-left+1);
         }
         return ans;
