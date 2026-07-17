fbc0943b54a16e6e51c745f1f23b62e884b4d4cd
Rudranil Mondal <rudranilmn1509@gmail.com>
2026-07-17 22:04:00 +0530

    Time: 0 ms (100.00%) | Memory: 8.8 MB (3.11%) - LeetSync

--- 2154-minimum-moves-to-convert-string/minimum-moves-to-convert-string.cpp
+++ 2154-minimum-moves-to-convert-string/minimum-moves-to-convert-string.cpp
@@ -1,19 +1,31 @@
 class Solution {
 public:
+    // int minimumMoves(string s) {
+    //     int l=0,r=0,count=0;
+    //     while(l<s.size()){
+    //         while(l<s.size() && s[l]=='O'){
+    //             l++;r++;
+    //         }
+    //         int existsX=0,lim=r+3;
+    //         while(r<lim && r<s.size()){
+    //             if(s[r]=='X')       existsX=1;
+    //             r++;  
+    //         }
+    //         if(existsX) count++;
+    //         l=r;
+    //     }
+    //     return count;
+    // }
     int minimumMoves(string s) {
-        int l=0,r=0,count=0;
-        while(l<s.size()){
-            while(l<s.size() && s[l]=='O'){
-                l++;r++;
+        int ans=0,i=0;
+        while(i<s.size()){
+            if(s[i]=='X'){
+                i+=3;
+                ans++;
             }
-            int existsX=0,lim=r+3;
-            while(r<lim && r<s.size()){
-                if(s[r]=='X')       existsX=1;
-                r++;  
-            }
-            if(existsX) count++;
-            l=r;
+            else i++;
         }
-        return count;
+        return ans;
     }
+    
 };
