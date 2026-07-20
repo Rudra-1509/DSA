3dceb7a1c49bb45352bd98810be652cfda41dda3
Rudranil Mondal <rudranilmn1509@gmail.com>
2026-07-20 17:22:03 +0530

    Time: 7 ms (53.58%) | Memory: 32.6 MB (39.79%) - LeetSync

--- 2159-two-out-of-three/two-out-of-three.cpp
+++ 2159-two-out-of-three/two-out-of-three.cpp
@@ -1,26 +1,20 @@
 class Solution {
 public:
+    void process(vector<int>& nums,unordered_map<int,int>& mpp){
+        unordered_set<int> vis;
+        for(int& num:nums){
+            if(vis.count(num))  continue;
+            vis.insert(num);
+            mpp[num]++;
+        }
+    }
+
     vector<int> twoOutOfThree(vector<int>& nums1, vector<int>& nums2, vector<int>& nums3) {
         vector<int> res;
         unordered_map<int,int> mpp;
-        unordered_set<int> vis;
-        for(int num:nums1){
-            if(vis.count(num))  continue;
-            vis.insert(num);
-            mpp[num]++;
-        }
-        vis.clear();
-        for(int num:nums2){
-            if(vis.count(num))  continue;
-            vis.insert(num);
-            mpp[num]++;
-        }
-        vis.clear();
-        for(int num:nums3){
-            if(vis.count(num))  continue;
-            vis.insert(num);
-            mpp[num]++;
-        }
+        process(nums1,mpp);
+        process(nums2,mpp);
+        process(nums3,mpp);
         for(auto it:mpp){
             if(it.second>=2)    res.push_back(it.first);
         }
