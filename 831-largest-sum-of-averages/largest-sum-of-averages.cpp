85c7c32dbe6a034914e58ae40b4058dd0e8b1f01
Rudranil Mondal <rudranilmn1509@gmail.com>
2026-07-21 19:57:22 +0530

    Time: 7 ms (33.66%) | Memory: 11.2 MB (56.20%) - LeetSync

--- 831-largest-sum-of-averages/largest-sum-of-averages.cpp
+++ 831-largest-sum-of-averages/largest-sum-of-averages.cpp
@@ -6,10 +6,10 @@ public:
         dp[0][0]=0;
         for(int k=1;k<=K;k++){
             for(int i=1;i<=n;i++){
-                double costAvg=0;
+                double costSum=0;
                 for(int j=i;j>0;j--){
-                    costAvg=1.0*costAvg*(i-j)+nums[j-1];
-                    costAvg/=(i-j+1);
+                    costSum+=nums[j-1];
+                    double costAvg=costSum/(i-j+1);
                     dp[i][k]=max(dp[i][k],dp[j-1][k-1]+costAvg);
                 }
             }
