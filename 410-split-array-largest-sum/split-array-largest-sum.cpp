5d8b78626bdffb687391b833c532d335b04aeab7
Rudranil Mondal <rudranilmn1509@gmail.com>
2026-07-21 18:58:33 +0530

    Time: 192 ms (3.48%) | Memory: 12.1 MB (6.30%) - LeetSync

--- 410-split-array-largest-sum/split-array-largest-sum.cpp
+++ 410-split-array-largest-sum/split-array-largest-sum.cpp
@@ -1,25 +1,41 @@
 class Solution {
 public:
-    bool isValid(vector<int>& nums,int mid, int k){
-        int sticks=0,sum=0;
-        for(int x:nums){
-            sum+=x;
-            if(sum>mid){
-                sticks++;
-                sum=x;
+    // bool isValid(vector<int>& nums,int mid, int k){
+    //     int sticks=0,sum=0;
+    //     for(int x:nums){
+    //         sum+=x;
+    //         if(sum>mid){
+    //             sticks++;
+    //             sum=x;
+    //         }
+    //     }
+    //     return sticks+1<=k;
+    // }
+
+    // int splitArray(vector<int>& nums, int k) {
+    //     int left=*max_element(nums.begin(),nums.end());
+    //     int right=accumulate(nums.begin(),nums.end(),0);
+    //     while(left<right){
+    //         int mid=left+(right-left)/2;
+    //         if(isValid(nums,mid,k)) right=mid;
+    //         else left=mid+1;
+    //     }
+    //     return left;
+    // }
+
+    int splitArray(vector<int>& nums, int K) {
+        int n=nums.size();
+        vector<vector<int>> dp(n+1,vector<int>(K+1,INT_MAX));
+        dp[0][0]=0;
+        for(int k=1;k<=K;k++){
+            for(int i=1;i<=n;i++){
+                int costSum=0;
+                for(int j=i;j>0;j--){
+                    costSum+=nums[j-1];
+                    dp[i][k]=min(dp[i][k],max(dp[j-1][k-1],costSum));
+                }
             }
         }
-        return sticks+1<=k;
-    }
-
-    int splitArray(vector<int>& nums, int k) {
-        int left=*max_element(nums.begin(),nums.end());
-        int right=accumulate(nums.begin(),nums.end(),0);
-        while(left<right){
-            int mid=left+(right-left)/2;
-            if(isValid(nums,mid,k)) right=mid;
-            else left=mid+1;
-        }
-        return left;
+        return dp[n][K];
     }
 };
