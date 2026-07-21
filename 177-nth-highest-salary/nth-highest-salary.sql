edce7992845720e1fac2fe44aeb565780a353d99
Rudranil Mondal <rudranilmn1509@gmail.com>
2026-07-21 16:06:07 +0530

    Time: 471 ms (43.70%) | Memory: 0B (100.00%) - LeetSync

--- 177-nth-highest-salary/nth-highest-salary.sql
+++ 177-nth-highest-salary/nth-highest-salary.sql
@@ -1,22 +1,31 @@
+-- CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
+-- BEGIN
+--   RETURN (
+--       # Write your MySQL query statement below.
+--             SELECT salary
+--             FROM(
+--                 SELECT 
+--                 salary,
+--                 DENSE_RANK() OVER(
+--                     ORDER BY salary DESC
+--                 ) AS rnk
+--                 FROM Employee e
+--             ) AS t
+--             WHERE t.rnk=N
+--             LIMIT 1
+--   );
+-- END
+
+
+
 CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
 BEGIN
+  SET N=N-1;
   RETURN (
-      # Write your MySQL query statement below.
-            SELECT salary
-            FROM(
-                SELECT 
-                salary,
-                DENSE_RANK() OVER(
-                    ORDER BY salary DESC
-                ) AS rnk
-                FROM Employee e
-            ) AS t
-            WHERE t.rnk=N
-            LIMIT 1
+    SELECT DISTINCT salary
+    FROM Employee
+    ORDER BY salary DESC
+    LIMIT 1
+    OFFSET N
   );
 END
-
-
-
-
-
