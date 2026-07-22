cbc94d8896aa2f0afdd44856c353376b54caf2cd
Rudranil Mondal <rudranilmn1509@gmail.com>
2026-07-22 17:31:45 +0530

    Time: 35 ms (95.88%) | Memory: 180.5 MB (69.70%) - LeetSync

--- 307-range-sum-query-mutable/range-sum-query-mutable.cpp
+++ 307-range-sum-query-mutable/range-sum-query-mutable.cpp
@@ -1,71 +1,116 @@
+// class NumArray {
+// private:
+//     struct node{
+//         int sum;
+//         int start;
+//         int end;
+//         node* left;
+//         node* right;
+//         node(int s,int e){
+//             start=s;end=e;
+//             sum = 0;
+//             left = nullptr;
+//             right = nullptr;
+//         }
+//     };
+
+//     node* root;
+//     node* builder(vector<int>& nums,int l,int r){
+//         node* newnode=new node(l,r);
+//         if(l==r){
+//             newnode->sum=nums[l];
+//             return newnode;
+//         }
+//         int mid=l+(r-l)/2;
+//         node* lhs=builder(nums,l,mid);
+//         node* rhs=builder(nums,mid+1,r);
+//         newnode->left=lhs;
+//         newnode->right=rhs;
+//         newnode->sum=lhs->sum+rhs->sum;
+//         return newnode;
+//     }
+
+//     int sumRange(node* root,int l,int r){
+//         if(!root)   return 0;
+//         if(r<root->start || l>root->end)   return 0;
+//         if(root->start>=l && root->end<=r)  return root->sum;
+//         int lhs=sumRange(root->left,l,r);
+//         int rhs=sumRange(root->right,l,r);
+//         return lhs+rhs;
+//     }
+
+//     int update(node* root,int index,int val){
+//         if(!root)   return 0;
+//         if(index<root->start || index>root->end)    return root->sum;
+//         if(root->start==root->end){
+//             root->sum=val;
+//             return val;
+//         }
+//         int lhs=update(root->left,index,val);
+//         int rhs=update(root->right,index,val);
+//         root->sum=lhs+rhs;
+//         return lhs+rhs;
+//     }
+
+// public:
+//     NumArray(vector<int>& nums) { 
+//         if(nums.empty())    root=nullptr;
+//         else    root=builder(nums,0,nums.size()-1);
+//     }
+    
+//     void update(int index, int val) {
+//         update(root,index,val);
+//     }
+    
+//     int sumRange(int left, int right) {
+//         return sumRange(root,left,right);   
+//     }
+// };
+
+
 class NumArray {
 private:
-    struct node{
-        int sum;
-        int start;
-        int end;
-        node* left;
-        node* right;
-        node(int s,int e){
-            start=s;end=e;
-            sum = 0;
-            left = nullptr;
-            right = nullptr;
+    int n;
+    vector<int> bit;
+    vector<int> nums;
+    int query(int i){
+        int ans=0;
+        for(;i>0;i-=i&(-i)){
+            ans+=bit[i];
         }
-    };
-
-    node* root;
-    node* builder(vector<int>& nums,int l,int r){
-        node* newnode=new node(l,r);
-        if(l==r){
-            newnode->sum=nums[l];
-            return newnode;
-        }
-        int mid=l+(r-l)/2;
-        node* lhs=builder(nums,l,mid);
-        node* rhs=builder(nums,mid+1,r);
-        newnode->left=lhs;
-        newnode->right=rhs;
-        newnode->sum=lhs->sum+rhs->sum;
-        return newnode;
+        return ans;
     }
-
-    int sumRange(node* root,int l,int r){
-        if(!root)   return 0;
-        if(r<root->start || l>root->end)   return 0;
-        if(root->start>=l && root->end<=r)  return root->sum;
-        int lhs=sumRange(root->left,l,r);
-        int rhs=sumRange(root->right,l,r);
-        return lhs+rhs;
-    }
-
-    int update(node* root,int index,int val){
-        if(!root)   return 0;
-        if(index<root->start || index>root->end)    return root->sum;
-        if(root->start==root->end){
-            root->sum=val;
-            return val;
+    void updateVal(int i,int x){
+        for(;i<=n;i+=i&(-i)){
+            bit[i]+=x;
         }
-        int lhs=update(root->left,index,val);
-        int rhs=update(root->right,index,val);
-        root->sum=lhs+rhs;
-        return lhs+rhs;
     }
 
 public:
-    NumArray(vector<int>& nums) { 
-        if(nums.empty())    root=nullptr;
-        else    root=builder(nums,0,nums.size()-1);
+    NumArray(vector<int>& nums) {
+        this->nums=nums;
+        n=nums.size();
+        bit.assign(n+1,0);
+        for(int i=1;i<=n;i++){
+            bit[i]+=nums[i-1];
+            int parent=i+(i&(-i));
+            if(parent<=n)
+                bit[parent]+=bit[i];
+        }
     }
     
     void update(int index, int val) {
-        update(root,index,val);
+        int diff=val-nums[index];
+        nums[index]=val;
+        updateVal(index+1,diff);
     }
     
-    int sumRange(int left, int right) {
-        return sumRange(root,left,right);   
+    int sumRange(int left, int right) { 
+        return query(right+1)-query(left);
     }
 };
+
+
 
 /**
  * Your NumArray object will be instantiated and called as such:
