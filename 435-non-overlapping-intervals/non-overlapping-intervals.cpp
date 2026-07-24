30f5b9d087fcb4392cf63c19bee957be191619ff
Rudranil Mondal <rudranilmn1509@gmail.com>
2026-07-24 23:38:36 +0530

    Time: 40 ms (78.82%) | Memory: 93.9 MB (69.14%) - LeetSync

--- 435-non-overlapping-intervals/non-overlapping-intervals.cpp
+++ 435-non-overlapping-intervals/non-overlapping-intervals.cpp
@@ -6,7 +6,7 @@ public:
                 return a[1]<b[1];
             });
         int end=intervals[0][1],keep=1,n=intervals.size();
-        for(int i=0;i<n;i++){
+        for(int i=1;i<n;i++){
             if(intervals[i][0]>=end){
                 keep++;
                 end=intervals[i][1];
