fe8553694483a3a4f4712586422a9d3ad258ad1a
Rudranil Mondal <rudranilmn1509@gmail.com>
2026-08-02 23:39:59 +0530

    Time: 7 ms (35.23%) | Memory: 19.6 MB (20.08%) - LeetSync

--- 909-stone-game/stone-game.cpp
+++ 909-stone-game/stone-game.cpp
@@ -6,7 +6,7 @@ public:
         vector<vector<int>> dp(n,vector<int>(n,0));
         for(int i=0;i<n;i++)    dp[i][i]=piles[i];
         for(int len=2;len<=n;len++){
-            for(int i=0;i<=len-n;i++){
+            for(int i=0;i<=n-len;i++){
                 int j=i+len-1;
                 int takeLeft=piles[i]-dp[i+1][j];
                 int takeRight=piles[j]-dp[i][j-1];
