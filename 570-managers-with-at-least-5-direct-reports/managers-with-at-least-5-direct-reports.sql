a142663d5715c093b76640adab94494b331c4478
Rudranil Mondal <rudranilmn1509@gmail.com>
2026-08-05 22:50:05 +0530

    Time: 397 ms (27.94%) | Memory: 0B (100.00%) - LeetSync

--- 570-managers-with-at-least-5-direct-reports/managers-with-at-least-5-direct-reports.sql
+++ 570-managers-with-at-least-5-direct-reports/managers-with-at-least-5-direct-reports.sql
@@ -1,15 +1,10 @@
 # Write your MySQL query statement below
-SELECT 
-    name 
-FROM
-(
-    SELECT
-        COUNT(e1.id) as cnt,
-        e2.name AS name
-    FROM Employee e1
-    INNER JOIN Employee e2
-    ON e1.managerId=e2.id
-    GROUP BY e1.managerId
-    HAVING cnt>=5
-) t;
+SELECT
+    e2.name AS name
+FROM Employee e1
+INNER JOIN Employee e2
+ON e1.managerId=e2.id
+GROUP BY e2.id,e2.name
+HAVING COUNT(e1.id)>=5
+
 
