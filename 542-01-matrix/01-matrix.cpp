b2ca395a451a64bea378fc90d3a7bceb66e80b1b
Rudranil Mondal <rudranilmn1509@gmail.com>
2026-08-05 10:07:48 +0530

    Time: 189 ms (5.05%) | Memory: 74.7 MB (5.14%) - LeetSync

--- 542-01-matrix/01-matrix.cpp
+++ 542-01-matrix/01-matrix.cpp
@@ -1,33 +1,37 @@
 class Solution {
 public:
-    bool isSafe(int i,int j,int r,int c){
-        return i>=0 && i<r && j>=0 && j<c;
+    vector<vector<int>> nav={{-1,0},{1,0},{0,1},{0,-1}};
+    bool isValid(int i,int j,int m,int n){
+        return i>=0 && j>=0 && i<m && j<n;
     }
-    vector<vector<int>> nav={{0,1},{0,-1},{1,0},{-1,0}};
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
-        int m=mat.size(),n=mat[0].size();
-        vector<vector<int>> res(m,vector<int>(n,-1));
+        int m=mat.size(),n=mat[0].size(),dist=-1;
+        vector<vector<int>> res(m,vector<int>(n,0));
         queue<pair<int,int>> q;
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 if(mat[i][j]==0){
                     q.push({i,j});
-                    res[i][j]=0;
+                    mat[i][j]=-1;
+                }
+            }
+        }
+        while(!q.empty()){
+            int sz=q.size();
+            dist++;
+            while(sz--){
+                auto [i,j]=q.front();q.pop();
+                res[i][j]=dist;
+                for(auto d:nav){
+                    int ni=i+d[0],nj=j+d[1];
+                    if(isValid(ni,nj,m,n) && mat[ni][nj]!=-1){
+                        q.push({ni,nj});
+                        mat[ni][nj]=-1;
+                    }
                 }
             }
         }
 
-        while(!q.empty()){
-            auto[i,j]=q.front();q.pop();
-            for(auto d:nav){
-                int ni=i+d[0];
-                int nj=j+d[1];
-                if(isSafe(ni,nj,m,n) && res[ni][nj]==-1){
-                    q.push({ni,nj});
-                    res[ni][nj]=res[i][j]+1;
-                }
-            }
-        }
         return res;
-    }      
+    }
 };
