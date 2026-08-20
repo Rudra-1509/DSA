f1340ecc611a370735deef0c92b959ccba042272
Rudranil Mondal <rudranilmn1509@gmail.com>
2026-08-20 19:41:38 +0530

    Time: 0 ms (100.00%) | Memory: 11 MB (18.98%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.cpp
+++ 213-house-robber-ii/house-robber-ii.cpp
@@ -1,23 +1,20 @@
 class Solution {
 public:
-    int helper(vector<int>& arr){
-        int n=arr.size();
-        if(n==1)    return arr[0];
+    int helper(int start,int end,vector<int>& nums){
+        int n=end-start+1;
+        if(n==1)    return nums[start];
         vector<int> dp(n,0);
-        dp[0]=arr[0];
-        dp[1]=max(arr[0],arr[1]);
+        dp[0]=nums[start];
+        dp[1]=max(dp[0],nums[start+1]);
         for(int i=2;i<n;i++)
-            dp[i]=max(dp[i-2]+arr[i],dp[i-1]);
+            dp[i]=max(dp[i-1],dp[i-2]+nums[i+start]);
         return dp[n-1];
     }
     int rob(vector<int>& nums) {
         int n=nums.size();
-        if(n==1) return nums[0];
-        vector<int> arr1,arr2;
-        for(int i=0;i<n;i++){
-            if(i!=0)        arr1.push_back(nums[i]);
-            if(i!=n-1)      arr2.push_back(nums[i]);
-        }
-        return max(helper(arr1),helper(arr2));
+        if(n==1)    return nums[0];
+        int takeFirst=helper(0,n-2,nums);
+        int takeLast=helper(1,n-1,nums);
+        return max(takeFirst,takeLast);    
     }
 };
